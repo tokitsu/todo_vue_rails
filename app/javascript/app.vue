ddb94a3f094031f625bda654d719d4ecd6f964f1
tokitsu <toki2222son@yahoo.co.jp>
2021-06-21 22:29:22 +0900

    add create function

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,11 +1,28 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
-    <ul>
-      <li v-for="memo in memos" :key="memo.id">
-        {{ memo.title }}： {{ memo.description }}
-      </li>
-    </ul>
+    <div class="form">
+      <div class="form-group">
+        <input v-model="title" placeholder="title" class="form-control" />
+      </div>
+      <div class="form-group">
+        <input
+          v-model="description"
+          placeholder="description"
+          class="form-control"
+        />
+      </div>
+      <button @click="addMemo">メモを追加</button>
+    </div>
+    <div class="flex">
+      <div v-for="memo in memos" :key="memo.id" class="card">
+        <div class="card-body">
+          <div class="card-title">
+            {{ memo.title }}
+          </div>
+          {{ memo.description }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,6 +34,8 @@ export default {
     return {
       message: "Hello Vue!",
       memos: "memos",
+      title: "",
+      description: "",
     };
   },
   mounted() {
@@ -26,13 +45,63 @@ export default {
     setMemo: function () {
       axios.get("/api/memos").then((response) => (this.memos = response.data));
     },
+    addMemo: function () {
+      axios
+        .post("/api/memos", {
+          title: this.title,
+          description: this.description,
+        })
+        .then((response) => this.setMemo());
+    },
   },
 };
 </script>
 
 <style scoped>
-p {
+<style lang="scss" scoped > p {
   font-size: 2em;
   text-align: center;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 32px;
+  &-group {
+    margin-bottom: 1rem;
+  }
+  &-control {
+    width: 1000px;
+    min-height: 24px;
+    font-size: 1rem;
+    border: 1px solid #ced4da;
+    padding: 4px 8px;
+  }
+}
+
+button {
+  width: 200px;
+}
+
+.flex {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.card {
+  width: 238px;
+  border: 1px solid rgba(0, 0, 0, 0.125);
+  border-radius: 0.25rem;
+  margin: 16px;
+  &-body {
+    padding: 1.25rem;
+  }
+  &-title {
+    margin-bottom: 0.75rem;
+    font-weight: 600;
+  }
 }
 </style>
